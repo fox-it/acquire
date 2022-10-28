bcdaf7d403dc362a7546382d72b589979b7de8b2
Miauwkeru <Miauwkeru@users.noreply.github.com>
2022-10-28 15:38:50 +0200

    Add typehints to esxi.py

--- acquire/esxi.py
+++ acquire/esxi.py
@@ -2,7 +2,6 @@ import logging
 import re
 import subprocess
 from contextlib import contextmanager
-from typing import Dict, List
 
 
 @contextmanager
@@ -19,20 +18,20 @@ def esxi_memory_context_manager():
 class EsxiMemoryManager:
     def __init__(self) -> None:
         self.group_id: str = ""
-        self.mem_scheme: Dict[str, str] = dict()
+        self.mem_scheme: dict[str, str] = dict()
 
-    def setup(self):
-        """"""
+    def setup(self) -> None:
+        """Change the ESXi memory limits of the group to unlimited."""
         self.group_id = self._get_group_id()
         self.mem_scheme = self._get_memory_scheme()
         self._set_memory_limits(max="unlimited", min_limit="unlimited")
 
-    def reset(self):
+    def reset(self) -> None:
         """Put memory limits back to their original values."""
         if self.mem_scheme:
             self._set_memory_limits(max=self.mem_scheme.get("max"), min_limit=self.mem_scheme.get("minLimit"))
 
-    def _execute_vsish_command(self, command: List[str]):
+    def _execute_vsish_command(self, command: list[str]) -> str:
         """Performs and logs a vsish command."""
         vsish_command = ["vsish", "-e"] + command
         logging.info(f"Executing '{' '.join(vsish_command)}' on ESXi host.")
@@ -41,7 +40,7 @@ class EsxiMemoryManager:
 
         return output.decode("utf-8")
 
-    def _get_group_id(self):
+    def _get_group_id(self) -> str:
         """Get the group ID of the current session."""
         group_id = self._execute_vsish_command(
             [
@@ -60,7 +59,7 @@ class EsxiMemoryManager:
 
         return group_id
 
-    def _set_memory_limits(self, max: str, min_limit: str):
+    def _set_memory_limits(self, max: str, min_limit: str) -> None:
         self._execute_vsish_command(
             [
                 "set",
@@ -70,7 +69,7 @@ class EsxiMemoryManager:
             ]
         )
 
-    def _get_memory_scheme(self):
+    def _get_memory_scheme(self) -> dict[str, str]:
         """Retrieve and parse the current memory scheme from an ESXi host."""
         mem_sched_allocation = self._execute_vsish_command(
             [
