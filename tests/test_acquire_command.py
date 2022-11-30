8f2834e443f4da3364ce9de72827fbc537a60144
Paul Moller <paul.moller@fox-it.com>
2022-11-30 02:22:09 -0800

    Process review feedback

--- tests/test_acquire_command.py
+++ tests/test_acquire_command.py
@@ -4,8 +4,8 @@ from unittest.mock import patch
 
 import pytest
 
-patch('ctypes.windll', create=True).start() # noqa
-from acquire.acquire import ( # noqa
+patch("ctypes.windll", create=True).start()
+from acquire.acquire import (  # noqa: E402
     CONFIG,
     MODULES,
     PROFILES,
