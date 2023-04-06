fe7896a95b55b3211a04d91bf54b03f4ff301362
pyrco <105293448+pyrco@users.noreply.github.com>
2023-04-06 09:18:27 +0200

    Collect GPO (Group Policy Objects) History
    (DIS-1747)

--- acquire/acquire.py
+++ acquire/acquire.py
@@ -825,6 +825,7 @@ class Misc(Module):
         ("dir", "sysvol/Windows/System32/WDI/LogFiles/StartupInfo"),
         ("dir", "sysvol/windows/sysvol/domain/policies/"),
         ("dir", "sysvol/windows/system32/GroupPolicy/DataStore/"),
+        ("dir", "sysvol/ProgramData/Microsoft/Group Policy/History/"),
         ("glob", "sysvol/Windows/System32/LogFiles/SUM/*.mdb"),
     ]
 
