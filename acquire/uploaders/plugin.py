dc5cca40a5d177e1077d7eb291578b229520e2f6
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2024-01-24 15:22:49 +0100

    Implement feedback.

--- acquire/uploaders/plugin.py
+++ acquire/uploaders/plugin.py
@@ -44,8 +44,8 @@ def upload_files_using_uploader(
     client = uploader.prepare_client(paths, proxies)
 
     counter = 0
-    plugin_gui = gui.GUI()
-    plugin_gui.progress = 55
+    upload_gui = gui.GUI()
+    upload_gui.progress = 55
 
     for path in paths:
         for retry in range(MAX_RETRIES):
@@ -64,6 +64,6 @@ def upload_files_using_uploader(
                 log.exception("")
 
         counter += 1
-        plugin_gui.progress = 55 + (counter / len(paths) * 40)
+        upload_gui.progress = 55 + (counter / len(paths) * 40)
 
     uploader.finish(client)
