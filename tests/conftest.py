3a4f5d018d04f086a53ab4d33ea832af079f20c4
pyrco <105293448+pyrco@users.noreply.github.com>
2024-02-09 13:39:24 +0100

    New collection and de-duplication logic
    When paths contain intermediate symlinks, these intermediate symlinks
    are collected instead of collecting the unresolved path as is.
    De-duplication between sysvol and it's equivalent drive letter path
    is done properly.
    Collecting of special files (e.g. MFT, Recyclebin etc.) is properly
    abstracted.
    
    (DIS-1222)

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,10 @@ def mock_fs(mock_file: BinaryIO) -> VirtualFilesystem:
     fs.map_file_entry("/foo/bar/own-file", VirtualFile(fs, "own-file", mock_file))
     fs.map_file_entry("/foo/bar/some-symlink", VirtualSymlink(fs, "some-symlink", "/foo/bar/some-file"))
     fs.map_file_entry("/foo/own-symlink", VirtualSymlink(fs, "own-symlink", "/foo/bar/own-file"))
+
+    fs.map_file_entry("/symlink/dir1", VirtualSymlink(fs, "dir1", "/symlink/dir2"))
+    fs.map_file_entry("/symlink/dir2/some-dir", VirtualSymlink(fs, "some-dir", "/symlink/dir3/some-dir"))
+    fs.map_file_entry("/symlink/dir3/some-dir/some-file", VirtualFile(fs, "some-file", mock_file))
     return fs
 
 
