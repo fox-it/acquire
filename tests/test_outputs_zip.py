45a6371290066f75e8673fe8310755309978d8bf
Roel de Jong <12800443+twiggler@users.noreply.github.com>
2024-10-16 11:00:38 +0200

    Test encryption in combination with zip

--- tests/test_outputs_zip.py
+++ tests/test_outputs_zip.py
@@ -6,6 +6,7 @@ import pytest
 from dissect.target.filesystem import VirtualFilesystem
 
 from acquire.outputs import ZipOutput
+from acquire.tools.decrypter import EncryptedFile
 
 
 @pytest.fixture(params=[(True, "deflate"), (True, "bzip2"), (True, "lzma"), (False, None)])
@@ -45,3 +46,26 @@ def test_zip_output_write_entry(mock_fs: VirtualFilesystem, zip_output: ZipOutpu
         assert stat.S_ISLNK(file_type)
     elif entry.is_file():
         assert stat.S_ISREG(file_type)
+
+
+@pytest.fixture
+def public_key() -> bytes:
+    with open("tests/data/public_key.pem", "r") as f:
+        return f.read()
+
+
+def test_zip_encrypt_write_entry(mock_fs: VirtualFilesystem, public_key: bytes, tmp_path: Path) -> None:
+    entry_name = "/foo/bar/some-file"
+    entry = mock_fs.get(entry_name)
+    zip_output = ZipOutput(tmp_path, compress=True, compression_method="bzip2", encrypt=True, public_key=public_key)
+    zip_output.write_entry(entry_name, entry)
+    zip_output.close()
+
+    encrypted_stream = EncryptedFile(zip_output.path.open("rb"), Path("tests/data/private_key.pem"))
+    decrypted_path = tmp_path / "decrypted.zip"
+    # Direct streaming is not an otion because zipfile needs seek when reading from encrypted files directly
+    with open(decrypted_path, "wb") as f:
+        f.write(encrypted_stream.read())
+    zip_file = zipfile.ZipFile(decrypted_path, mode="r")
+
+    assert entry.open().read() == zip_file.open(entry_name).read()
