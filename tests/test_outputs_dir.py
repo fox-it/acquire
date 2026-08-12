fdd7c1e7f78457864ad8f56f33a9f4145536d17c
Miauwkeru <Miauwkeru@users.noreply.github.com>
2026-08-12 09:55:43 +0200

    Update tests for the targetpath and filesystem changes in dissect.target (#312)

--- tests/test_outputs_dir.py
+++ tests/test_outputs_dir.py
@@ -58,7 +58,7 @@ def test_dir_output_write_entry(mock_fs: VirtualFilesystem, dir_output: Director
     file = files[0]
 
     # Convert a os seperated file to the entry name.
-    file_path = f"/{normalize(str(file.relative_to(path)), alt_separator=os.sep)}"
+    file_path = f"/{normalize(str(file.relative_to(path)), sep=os.sep)}"
     assert file_path == entry_name
 
     if entry.is_dir():
